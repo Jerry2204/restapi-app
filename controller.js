aae4b3a5e3b7bd2e744bc2c755449e80c9f3594c
jerry2204 <jerryandrianto22@gmail.com>
2020-12-16 21:35:45 +0700

    show data mahasiswa

--- controller.js
+++ controller.js
@@ -6,3 +6,17 @@ const connection = require('./koneksi');
 exports.index = (req, res) => {
     response.ok("Aplikasi Rest API berjalan!", res);
 }
+
+// menampilkan semua data mahasiswa
+
+exports.tampilDataMhs = (req, res) => {
+    connection.query(
+        'SELECT * FROM mahasiswa',
+        (error, rows, fields) => {
+            if(error){
+                connection.log(error);
+            }else{
+                response.ok(rows, res);
+            }
+        });
+}
