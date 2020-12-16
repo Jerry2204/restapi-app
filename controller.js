985ed2996e075d2e443cdc3770baee5dfaa368dc
jerry2204 <jerryandrianto22@gmail.com>
2020-12-16 20:55:49 +0700

    create router and update controller

--- controller.js
+++ controller.js
@@ -4,5 +4,5 @@ const response = require('./res');
 const connection = require('./koneksi');
 
 exports.index = (req, res) => {
-    res.ok("Aplikasi Rest API berjalan!");
+    response.ok("Aplikasi Rest API berjalan!");
 }
